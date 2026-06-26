7e9feb5801b2b0f5f91dbec11bd0f4417f6d3b5e
Mohit Patil <mohitpatilaiml@gmail.com>
2026-06-26 06:57:40 +0530

    test git

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## git commands
 ### git add .
 ### git commit -m 'any message'
+
+### what is git?
+### git is a version control which track the changes
+
+### what is API?
+### dont know
+
+### what is github?
--- test.py
+++ test.py
@@ -20,9 +20,9 @@
 # print(x[0:3:2]) # Start:end:step
 # print(x[::-1]) # reverse string
 
-for i in range(1,10):
-    if i%2==0:
-        print(i)
+# for i in range(1,10):
+#     if i%2==0:
+#         print(i)
 
-for i in range(0,10,2):
-        print(i)
+# for i in range(0,10,2):
+#         print(i)
